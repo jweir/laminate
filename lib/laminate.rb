da99c2a72b5a625cfb153b4cea83adc9e50a08a2
J. Weir <john@smokingun.com>
2009-12-21 12:41:48 -0500

    Allow ENV["LIB_LUA"] to find a file such as liblua.5.1.so

--- lib/laminate.rb
+++ lib/laminate.rb
@@ -1,7 +1,7 @@
 # Laminate
 def find_lib
   directory = File.expand_path(File.dirname(__FILE__) + '/../lua/')
-  ENV['LUA_LIB'] = [Dir.glob("#{directory}/liblua*.*")].flatten.first
+  ENV['LUA_LIB'] = [Dir.glob("#{directory}/liblua**.*")].flatten.first
 end
 
 find_lib
