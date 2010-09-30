34037d83cb20108614f536e1011fa98a00669a88
J. Weir <john@smokinggun.com>
2010-09-29 22:11:09 -0400

    Merged security test into State

--- test/laminate/state_test.rb
+++ test/laminate/state_test.rb
@@ -12,6 +12,12 @@ class Laminate::StateTest < Test::Unit::TestCase
     end
   end
 
+  def should_raise_error(code)
+    assert_raise Laminate::TemplateError, "#{code} should have raised an error" do
+      Laminate::Template.new(:text => code, :logger => @logger).render(:raise_errors => true)
+    end
+  end
+
   context "options" do
     setup do
       @state = State.new(
@@ -96,26 +102,7 @@ class Laminate::StateTest < Test::Unit::TestCase
   end
 
   context "timeouts" do
-    setup do
-      @loop = %{for i=1,1e12 do f = 'hello'; end}
-    end
-
-    should "use the default timeout" do
-      start = Time.now
-      assert_raise Rufus::Lua::LuaError do
-        State.new.run {|s| s.eval(@loop)}
       end
-      assert_in_delta 15.0, (Time.now - start), 0.5
-    end
-
-    should "use a given timeout" do
-      start = Time.now
-      assert_raise Rufus::Lua::LuaError do
-        State.new(:timeout => 3).run {|s| s.eval(@loop)}
-      end
-      assert_in_delta 3.0, (Time.now - start), 0.5
-    end
-  end
 
   context "sandbox" do
     %w{arg dofile loadfile os package require}.each do |sandboxed_method|
@@ -125,4 +112,65 @@ class Laminate::StateTest < Test::Unit::TestCase
     end
   end
 
+  context "security" do
+
+    setup do
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::FATAL
+    end
+
+    should "laminate_excludes_lua_builtin_funcs" do
+      should_raise_error "<%= assert(loadstring('x = 1'))() %>"
+      should_raise_error "<% package.path %>"
+      should_raise_error "<% os.clock() %>"
+      should_raise_error "<% os.execute('ls') %>"
+      should_raise_error "<% io.stdout:write('hello world') %>"
+      should_raise_error "<% require ('io') %>"
+      should_raise_error "<% dofile('fixtures/snippet.lua') %>"
+      should_raise_error "<% collectgarbage('stop') %>"
+      should_raise_error "<% x = #_G %>"
+      should_raise_error "<% x = getfenv(0) %>"
+      should_raise_error "<% y = getmetatable('') %>"
+      should_raise_error "<% setfenv(1, {}) %>"
+      should_raise_error "<% setmetatable(string, {}) %>"
+      should_raise_error "<% assert(loadfile('barx.lua'))%>"
+      # We disable string:rep because it can use too much memory
+      should_raise_error "<% if string.rep(' ', 3) ~= '   ' then error('overridde'); end %>"
+    end
+
+    should "not allow getmetatable" do
+      should_raise_error '<% getmetatable("foo").__index.upper = function() return "fail" end; %> <%string.upper("bar")%>'
+    end
+
+    context "the alarm" do
+      setup do
+        @loop = %{for i=1,1e12 do f = 'hello'; end}
+      end
+
+      should "should abort a long running script with the default timeout" do
+        start = Time.now
+        assert_raise Rufus::Lua::LuaError do
+          State.new.run {|s| s.eval(@loop)}
+        end
+        assert_in_delta 15.0, (Time.now - start), 0.5
+      end
+
+      should "use a given timeout" do
+        start = Time.now
+        assert_raise Rufus::Lua::LuaError do
+          State.new(:timeout => 1).run {|s| s.eval(@loop)}
+        end
+        assert_in_delta 1.0, (Time.now - start), 0.5
+      end
+
+      should "close the state" do
+        state = State.new(:timeout => 1)
+        state.expects(:close)
+        assert_raise Rufus::Lua::LuaError do
+          state.run {|s| s.eval(@loop) }
+        end
+      end
+    end
+
+  end
 end
