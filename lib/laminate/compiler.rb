ff2fc9355c3a258e5c82ba571ffe6354aedbdd90
J. Weir <john@smokinggun.com>
2010-11-11 15:37:40 -0500

    Compiler to V8

--- lib/laminate/compiler.rb
+++ lib/laminate/compiler.rb
@@ -3,50 +3,48 @@ module Laminate
 
     # Compiles an HTML-Lua template in a Lua script.
     def compile(name, source)
-      # lua is an array of source lines. Each element is one line of generated Lua code.
-      # These will be concatenated at the end to return the full lua script.
-      lua = []
+      out = []
 
-      lua << "function #{lua_template_function(name)}()"
+      out << "function #{out_template_function(name)}() {"
       # LUA: The _out var is an array (table). Each text fragment is appended
       # to the array. Thus the array serves like the _erbout string buffer in ERB.
       # At the end, all the fragments are joined together to return the result.
-      lua << 'local _out = {}; function out(s) table.insert(_out, tostring(s)); end; '
+      out << 'var _out = []; function out(s){ _out.push(s)};'
 
       source.each do |element|
         case element.first
-        when :text then add_text(element.last, lua)
-        when :code then add_code(element.last, lua)
-        when :print then add_print(element.last, lua)
+        when :text then add_text(element.last, out)
+        when :code then add_code(element.last, out)
+        when :print then add_print(element.last, out)
         else raise "Unknown template kind #{element.inspect}"
         end
       end
 
-      lua.pop if lua.last == "\n" || lua.last == ''
-      lua << "return table.concat(_out)"
-      lua << "end"
-      lua.join("\n")
+      out.pop if out.last == "\n" || out.last == ''
+      out << "return _out.join('');"
+      out << "}"
+      out.join("\n")
     end
 
-    def lua_template_function(name)
+    def out_template_function(name)
       "_template_#{name}".gsub(/[ \.]/,'_')
     end
 
     protected
 
-    def add_text(text, lua)
-      lua << "table.insert(_out, [===[#{text}]===]);"
+    def add_text(text, out)
+      out << "_out.push('#{text.gsub(/'/im,"\\\\'").gsub(/\n/im,"\\n")}');"
     end
 
-    def add_code(code, lua)
+    def add_code(code, out)
       code.each_line do |line|
-        lua.last << "#{line}"
+        out.last << "#{line.rstrip};"
       end
     end
 
-    def add_print(code, lua)
+    def add_print(code, out)
       code.each_line do |line|
-        lua.last << "table.insert(_out, #{line});"
+        out.last << "_out.push(#{line.rstrip});"
       end
     end
 
